64cbc91e8e1b77d0b185163fb650bc1c12827ff9
Lola Chevallier <lolachevallier@laposte.net>
2025-05-26 20:30:48 +0200

    Updates

---
+++ precache-manifest.3742175fb60add08e95f5c69a1ad0c0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a67561abb36e42368e5436e69a2f530",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "38528ce431dc28b391f3",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2c20232a71182934efc9",
+    "url": "/Portfolio/static/css/main.4f2ccb36.chunk.css"
+  },
+  {
+    "revision": "38528ce431dc28b391f3",
+    "url": "/Portfolio/static/js/2.00701bcc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.00701bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c20232a71182934efc9",
+    "url": "/Portfolio/static/js/main.00ed3134.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
